aff519c9c3e5d6288d69875b6f1ffd14c7a02a70
Hoshi Desu <diamondassasinzz@gmail.com>
2023-04-27 20:31:20 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from './components/Navbar/Navbar'
+//angelTechnologies ftw
+const App = () => {
+  return (
+    <div>
+      {/* reference for calling component */}
+      <Navbar />
+    </div>
+  )
+}
+
+export default App
